7ab0ab1d02304bdf9fdd897ab1d1d735137d6f4d
yananob <yananob@gmail.com>
2026-08-12 14:57:33 +0900

    update

--- tests/listen_local_event.sh
+++ tests/listen_local_event.sh
@@ -2,11 +2,11 @@
 set -eu
 
 source ./tests/secrets.sh
-source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
+source ./_myapps-common/test/export_secrets.sh ${SECRETS[*]}
 
 # Launch function
 export FUNCTION_TARGET=main_event
 export FUNCTION_SIGNATURE_TYPE=cloudevent
 APP_ENV=local php -S localhost:8081 vendor/bin/router.php
 
-source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
+source ./_myapps-common/test/unset_secrets.sh ${SECRETS[*]}
--- tests/listen_local_http.sh
+++ tests/listen_local_http.sh
@@ -2,11 +2,11 @@
 set -eu
 
 source ./tests/secrets.sh
-source ./_cf-common/test/export_secrets.sh ${SECRETS[*]}
+source ./_myapps-common/test/export_secrets.sh ${SECRETS[*]}
 
 # Launch function
 export FUNCTION_TARGET=main_http
 APP_ENV=local php -S localhost:${PORT:-8080} vendor/bin/router.php
 
 
-source ./_cf-common/test/unset_secrets.sh ${SECRETS[*]}
+source ./_myapps-common/test/unset_secrets.sh ${SECRETS[*]}
